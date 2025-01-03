95997b68e7a7aad8b69088a677e568f4590ac1a3
ntua-el21139 <el21139@mail.ntua.gr>
2025-01-03 19:16:17 +0200

    paths

--- lib/presentation/alkis/leaderboard_screen/provider/leaderboard_provider.dart
+++ lib/presentation/alkis/leaderboard_screen/provider/leaderboard_provider.dart
@@ -6,34 +6,34 @@ class LeaderboardProvider extends ChangeNotifier {
   final List<List<ChallengeItemModel>> challengePages = [
     [
       ChallengeItemModel(
-        imageUrl: 'assets/images/avocado.svg',
+        imageUrl: 'assets/images/avocado.png',
         title: 'Avocado\nchallenge',
         backgroundColor: Color(0xFF4CD964),
       ),
       ChallengeItemModel(
-        imageUrl: 'assets/images/banana.svg',
+        imageUrl: 'assets/images/banana.png',
         title: 'Banana\nchallenge',
         backgroundColor: Color(0xFFFFCC00),
       ),
       ChallengeItemModel(
-        imageUrl: 'assets/images/meat.svg',
+        imageUrl: 'assets/images/meat.png',
         title: 'Carnivore\nchallenge',
         backgroundColor: Color(0xFFFF3B30),
       ),
     ],
     [
       ChallengeItemModel(
-        imageUrl: 'assets/images/beatles.svg',
+        imageUrl: 'assets/images/beatles.png',
         title: 'Beatles\nchallenge',
         backgroundColor: Color(0xFF007AFF),
       ),
       ChallengeItemModel(
-        imageUrl: 'assets/images/broccoli.svg',
+        imageUrl: 'assets/images/broccoli.png',
         title: 'Broccoli\nchallenge',
         backgroundColor: Colors.black,
       ),
       ChallengeItemModel(
-        imageUrl: 'assets/images/wrap.svg',
+        imageUrl: 'assets/images/wrap.png',
         title: 'Wrap\nchallenge',
         backgroundColor: Color(0xFF9747FF),
       ),
