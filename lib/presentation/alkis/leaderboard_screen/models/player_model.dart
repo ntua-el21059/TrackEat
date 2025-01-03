95b32b1827a86858af270438a521ce662324110c
ntua-el21139 <el21139@mail.ntua.gr>
2025-01-03 18:23:59 +0200

    something

---
+++ lib/presentation/alkis/leaderboard_screen/models/player_model.dart
@@ -0,0 +1,11 @@
+class Player {
+  final String name;
+  final String imageUrl;
+  final int points;
+
+  Player({
+    required this.name,
+    required this.imageUrl,
+    required this.points,
+  });
+}
