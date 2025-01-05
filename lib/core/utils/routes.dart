893f03131bcb22874c43221c0fc45c676723fcaa
ntua-el21139 <el21139@mail.ntua.gr>
2025-01-05 18:47:16 +0200

    an error on routes.dart

--- lib/core/utils/routes.dart
+++ lib/core/utils/routes.dart
@@ -1,3 +1,3 @@
-  static String socialProfileMyselfScreen = '/social_profile_myself_screen';
-  static String socialProfileViewScreen = '/social_profile_view_screen';
-  static String findFriendsScreen = '/find_friends_screen'; 
+String socialProfileMyselfScreen = '/social_profile_myself_screen';
+String socialProfileViewScreen = '/social_profile_view_screen';
+String findFriendsScreen = '/find_friends_screen';
